3f7f8d67bf6e8f85a7ae9c465d2029236177fc86
Daniel Esteban Yepes <deyepes@gmail.com>
2021-04-04 11:59:00 -0500

    initial projects for refactoring

---
+++ Calculadora/src/main/java/main.java
@@ -0,0 +1,13 @@
+public class main {
+
+    public static int f(int x, int y){
+        return x+y;
+    }
+
+    public static void main(String args[]){
+
+        int x=5, y=3, z;
+        z=f(x, y);
+        System.out.println(z);
+    }
+}
